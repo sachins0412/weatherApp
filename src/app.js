30408dd36c4005735ce942b17c8bdf50c3803c98
sachins0412 <saxenas964@gmail.com>
2022-06-30 23:24:41 +0530

    set up handleBars for view pages

--- src/app.js
+++ src/app.js
@@ -1,33 +1,48 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-
-const path = require("path");
+const hbs = require("hbs");
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./../templates/views"));
+hbs.registerPartials(path.join(__dirname, "./../templates/partials"));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Weather App",
-    name: "someone",
+    name: "Gippi",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About",
-    name: "someone",
+    name: "Gippi",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    title: "Help",
+    name: "Gippi",
     message: "something to help",
   });
 });
 
-// app.get("/weather", (req, res) => {
-//   res.send("weather");
-// });
+app.get("/help/*", (req, res) => {
+  res.render("notFound", {
+    title: 404,
+    message: "help article not found",
+    name: "Gippi",
+  });
+});
+app.get("*", (req, res) => {
+  res.render("notFound", {
+    title: 404,
+    message: "Page not found",
+    name: "Gippi",
+  });
+});
 
 app.listen(3000, () => {
   console.log("server up!");
