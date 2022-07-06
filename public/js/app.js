0093046a87769c5ff643f40b21947ed4f7495cfa
sachins0412 <saxenas964@gmail.com>
2022-07-06 22:06:24 +0530

    setup URL for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,14 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   messageOne.textContent = "Loading";
   messageTwo.textContent = "";
-  fetch(`http://localhost:3000/weather?address=${address.value}`).then(
-    (res) => {
-      res.json().then((response) => {
-        if (response.error) {
-          messageOne.textContent = response.error;
-        } else {
-          messageOne.textContent = response.location;
-          messageTwo.textContent = response.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${address.value}`).then((res) => {
+    res.json().then((response) => {
+      if (response.error) {
+        messageOne.textContent = response.error;
+      } else {
+        messageOne.textContent = response.location;
+        messageTwo.textContent = response.forecast;
+      }
+    });
+  });
 });
